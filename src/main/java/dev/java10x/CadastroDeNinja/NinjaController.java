aaf9afadfb3549903443bbe3b73621a2e232afd7
Ramon Freitas <ramonfreitas144@gmail.com>
2025-04-22 00:49:56 -0300

    primeira issue criada

--- src/main/java/dev/java10x/CadastroDeNinja/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinja/NinjaController.java
@@ -4,9 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+* Anotações importante para controllers
+*/
 @RestController // anotação para controlar a API Rest, passando um controlador
 @RequestMapping // sempre anda junto com o RestController, coloca todas as rotas no mesmo lugar
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasvindas") // anotação para chamar informações da rota, como estamos chmando localhost:8080 , /boasvindas e minusculo!
     public String boasVindas(){
---
+++ src/main/java/dev/java10x/CadastroDeNinja/NinjaModel.java
@@ -0,0 +1,50 @@
+package dev.java10x.CadastroDeNinja;
+
+import jakarta.persistence.*;
+
+// JPA - Java Persistence API
+@Entity // transformo toda a classe numa entidade do meu banco de dados, basicamente aviso ao sistema que irei criar uma tabela no banco de dados com ostributos dessa classe.
+@Table(name = "tb_cadastro") // dando nome para tabela de cadastro de ninjas, usamos o @Table. como boas práticas colocamos o nome do jeito que está aí.
+
+public class NinjaModel { // nossa classe de atributos, ou seja, nosso modelos de ninjas
+
+    @Id // serve para gerar o ID automáticamente para a tabela de cadastro de ninja
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // a estratégia como os IDs seram gerados. anda junto com o @Id. nesse caso serão números sequenciais.
+    long id; // diferencia os ninjas por para serem únicos dentro do banco de dados. Não precisa criar um construtor para ele. encrementa automático
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
