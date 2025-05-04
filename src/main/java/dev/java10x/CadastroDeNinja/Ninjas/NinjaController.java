07d235582dfa8554780e39f8c84116e518ac9159
Ramon Freitas <ramonfreitas144@gmail.com>
2025-05-04 13:52:37 -0300

    feat: Atualiza o controller com o mock de dados

--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
@@ -1,11 +1,10 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /*
 * Anotações importante para controllers
+* EndPoint - é a class mais próxima do usuário
 */
 @RestController // anotação para controlar a API Rest, passando um controlador
 @RequestMapping // sempre anda junto com o RestController, coloca todas as rotas no mesmo lugar
@@ -16,4 +15,34 @@ public class NinjaController {
         return "Essa é a minha primeira mensagem nessa rota";
     }
 
+
+    // Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // Procurar todos os Ninja (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Mostrar Ninjas";
+    }
+
+    // Mostrar ninjas por id (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId() {
+        return "Ninja criado por id";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar ninja por id";
+    }
+
+    // Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Deletar ninja por id";
+    }
 }
--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaRepository.java
@@ -1,4 +1,4 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
 
-public class NinjaRepository {
+public interface NinjaRepository {
 }
