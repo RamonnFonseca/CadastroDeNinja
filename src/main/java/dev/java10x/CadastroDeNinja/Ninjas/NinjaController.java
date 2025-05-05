017911b5e85be4969f8e54f751eefd5e5cb403ef
Ramon Freitas <ramonfreitas144@gmail.com>
2025-05-05 00:45:50 -0300

    feat: Alterar rota do controller para listar os ninjas e injeta a dependencia do service e do repository

--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
@@ -2,6 +2,8 @@ package dev.java10x.CadastroDeNinja.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /*
 * Anotações importante para controllers
 * EndPoint - é a class mais próxima do usuário
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/ninjas") // sempre anda junto com o RestController, coloca todas as rotas no mesmo lugar
 public class NinjaController {
 
+    private NinjaService ninjaService; // Injetando a dependência do NinjaService
+    // instacio o ninja service aqui com o um construtor também para ter acesso a regra de negócios.
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
     @GetMapping("/boasvindas") // anotação para chamar informações da rota, como estamos chmando localhost:8080 , /boasvindas e minusculo!
     public String boasVindas(){
+
         return "Essa é a minha primeira mensagem nessa rota";
     }
 
@@ -19,19 +28,21 @@ public class NinjaController {
     // Adicionar ninja (CREATE)
     @PostMapping("/criar")
     public String criarNinja() {
+
         return "Ninja criado";
     }
 
     // Procurar todos os Ninja (READ)
     @GetMapping("/listar")
-    public String mostrarTodosOsNinjas(){
-        return "Mostrar Ninjas";
+    public List<NinjaModel> listarNinjas(){
+        return ninjaService.listarNinjas(); // pegando a função listarNinjas do meu ninja service
     }
 
     // Mostrar ninjas por id (READ)
     @GetMapping("/listarID")
     public String mostrarTodosOsNinjasPorId() {
-        return "Ninja criado por id";
+
+        return "Mostrar Ninja por id";
     }
 
     // Alterar dados dos ninjas (UPDATE)
--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaService.java
@@ -1,4 +1,21 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
 
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+// faremos na camada service as regras de negocios
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+    // inicializo um construtor normal quando for trabalhar com a injeção de dependência, ou serviços.
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // Listar todos os meus ninjas
+    public List<NinjaModel> listarNinjas() {
+        return ninjaRepository.findAll();
+    }
 }
