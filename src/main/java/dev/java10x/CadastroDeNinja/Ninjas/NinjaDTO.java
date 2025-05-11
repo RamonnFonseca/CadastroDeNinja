eee2e990de5af7047def31b8cd3eefdd5cd2f697
Ramon Freitas <ramonfreitas144@gmail.com>
2025-05-10 22:39:15 -0300

    Chore: Inclui a versao DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
+import dev.java10x.CadastroDeNinja.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class NinjaDTO {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NinjaDTO { //Basicamente é uma cópia do NinjaModel(entidade do banco de dados) só que não tem acesso ao banco de dados. Usado como proteção
+    // Não preciso colocas as anotações de tabela porque o DTO não tem acesso ao banco de dados, logo, posso tirar todas as anotações de banco de dados
+    // DTO tira a responsabilidade da entidade
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private MissoesModel missoes;
+    private String rank;
+
 }
